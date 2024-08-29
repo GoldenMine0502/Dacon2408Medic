5344625650831223480655d2dcede804ca3a053c
GoldenMine0502 <ehe123@naver.com>
2024-08-29 21:19:56 +0900

    maxlen

--- pubchem2/maxlen.py
+++ pubchem2/maxlen.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
+PRETRAIN_FILTERED_PATH = '../dataset/filtered_pubchemchembl.tsv'
 TEST_PATH = '../dataset/test.csv'
+
+data = pd.read_csv(PRETRAIN_FILTERED_PATH, sep='\t')
 test_data = pd.read_csv(TEST_PATH)
 
+pretrain_smiles = data['SMILES']
 finetune_test_smiles = test_data['Smiles']
 
-print(len(finetune_test_smiles.max()))
+print(len(pretrain_smiles.max()), len(finetune_test_smiles.max()))
